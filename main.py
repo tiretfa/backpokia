8eb6212b1adbfd99efd8762d1bedde734944951b
tiretfa <tiretfabien@hotmail.fr>
2025-12-28 19:59:00 +0100

    make endpoint to request to mistral

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import os
 from typing import Union
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from mistralai import Mistral
+
+api_key = os.environ["MISTRAL_API_KEY"]
+model = "mistral-small-latest"
+
+client = Mistral(api_key=api_key)
 
 app = FastAPI()
 
@@ -37,3 +44,19 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.post("/chatbot")
+def ask_chatbot(data: dict):
+    chat_response = client.chat.complete(
+        model= model,
+        messages = [
+            {
+                "role": "user",
+                "content": data['q'],
+            },
+        ],
+        stream=False
+    )
+    answer = chat_response.choices[0].message.content
+    return {"answer": answer}
+
